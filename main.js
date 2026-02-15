c3081b4f2f08a9766601fe00f14759728f05872d
Fran <140926702+FranTheCaveman@users.noreply.github.com>
2026-02-15 19:40:43 +1300

    Fix bugs occuring after getting a result, where "num1 +" and entering num2 causes undefined variables, and num1 being blank after selecting an operator (due to num1 not being assigned the result)

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+// TODO:
+// Fix overflow
+// resolve . to 0. if not followed by any numbers
+// infinity when dividing by 0 (add a message or something)
+// Support for negative numbers when using - before a num1
+// backspace
+// keyboard support
+
 let operationDisplay = document.querySelector(".operation-display");
 let resultDisplay = document.querySelector(".result-display");
 let clearButton = document.querySelector(".clear");
@@ -11,7 +19,6 @@ let operatorLocked = true; // If operator is locked it is because num1 is blank
 let equalsLocked = true; // If equals is locked it is because: num1 is not selected or I selected an operator and num2 is blank
 let result = "";
 let decimalLocked = false;
-let numbersLocked = false;
 
 function add(num1, num2) {
     return parseFloat(num1) + parseFloat(num2);
@@ -57,24 +64,27 @@ let numbers = document.querySelectorAll("[data-num], .decimalPoint");
 for (const num of numbers) {
     num.addEventListener("click", (event) => {
         let val = num.getAttribute("data-num");
-        if (val === ".") decimalLocked = true;
-        updateDisabledButtons();
+        result = ""; // Clear result if user presses num after calculating answer
 
-        // Num1 should append if operator & num2 isn't yet selected, and if there is no result
-        if (operatorLocked || operator === "" && result === "") {
-            num1 += val;
-            operatorLocked = false;
+        if (!(val === "." && decimalLocked)) {
+            // Num1 should append if operator & num2 isn't yet selected, and if there is no result
+            if (operatorLocked || operator === "" && result === "") {            
+                num1 += val;
+                operatorLocked = false;
+            }
+            // Num2 should append if an operator has been selected
+            else {
+                num2 += val;
+                // Equals is unlocked when num2 is selected
+                equalsLocked = false;
+            }
+            updateOperationDisplay(num1, operatorSymbol, num2);
+            console.log("op locked", operatorLocked);
+            console.log("equal locked",equalsLocked);
+
+            if (val === ".") decimalLocked = true;
         }
-        // Num2 should append if an operator has been selected
-        else {
-            num2 += val;
-            // Equals is unlocked when num2 is selected
-            equalsLocked = false;
-            updateDisabledButtons();
-        }
-        updateOperationDisplay(num1, operatorSymbol, num2);
-        console.log(num1);
-        console.log(num2);
+        updateDisabledButtons();
     });
 }
 
@@ -84,49 +94,50 @@ for (const op of operators) {
     op.addEventListener("click", (event) => {
         let notation = op.getAttribute("data-notation");
         let notationSymbol = op.innerHTML;
-        numbersLocked = false;
 
+        // When User selected num1 then an operator
         if (!operatorLocked && equalsLocked) {
             if (notation != "=") {
                 operator = notation;
                 operatorSymbol = notationSymbol;
-                decimalLocked = false;
-                updateDisabledButtons();
+                if (result !== "") num1 = result;
             };
             console.log(operator)
         }
+        // When user selected num1, operator, num2 then an operator
         else if (!operatorLocked && !equalsLocked) {
-            result = operate(operator, num1, num2);
+            result = operate(operator, num1, num2) + "";
             resultDisplay.textContent = result;
             num1 = result;
 
             if (notation === "=") {
                 operator = "";
                 operatorSymbol = "";
-                decimalLocked = true;
-                numbersLocked = true;
+                num1 = "";
+                num2 = "";
+                operatorLocked = false;
             }
             else {
                 operator = notation; // Change notation
-                decimalLocked = false;
+                operatorSymbol = notationSymbol;
             }
 
             num2 = "";
             equalsLocked = true;
-            
-            updateDisabledButtons();
         }
-        operatorSymbol = notationSymbol;
+        
+        decimalLocked = false;
         updateOperationDisplay(num1, operatorSymbol, num2);
+        updateDisabledButtons();
+
+        console.log("op locked", operatorLocked);
+        console.log("equal locked",equalsLocked);
     })
 }
 
 function updateDisabledButtons() {
     equalsButton.disabled = equalsLocked;
     decimalButton.disabled = decimalLocked;
-    for (const num of numbers) {
-        num.disabled = numbersLocked;
-    }
 }
 
 updateDisabledButtons();
@@ -139,9 +150,11 @@ clearButton.addEventListener("click", (event) => {
     operatorSymbol = "";
     operatorLocked = true;
     equalsLocked = true;
-    numbersLocked = false;
+    decimalLocked = false;
     result = "";
     operationDisplay.textContent = " ";
     resultDisplay.textContent = "0";
     updateDisabledButtons();
 })
+
+// After a result, going 5 + and then entering final num results in undefined num1
